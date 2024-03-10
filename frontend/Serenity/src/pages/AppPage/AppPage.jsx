8bc18d65c021e533208ebf9176a96d806ab63565
Alexandru Belascu <alex15belascu@gmail.com>
2024-03-10 06:45:19 +0200

    the charts and fixed some bugs

--- frontend/Serenity/src/pages/AppPage/AppPage.jsx
+++ frontend/Serenity/src/pages/AppPage/AppPage.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import './AppPage.css';
-import { Text, Card, Inset } from '@radix-ui/themes';
-import { useNavigate } from 'react-router-dom';
-import { Button, Dialog, Table, Flex } from '@radix-ui/themes';
-import JournalView from '../../components/JournalView/JournalView.jsx';
-import JournalEntry from '../../components/JournalEntry/JournalEntry.jsx';
+import React, { useState } from "react";
+import "./AppPage.css";
+import { Text, Card, Inset } from "@radix-ui/themes";
+import { useNavigate } from "react-router-dom";
+import { Button, Dialog, Table, Flex } from "@radix-ui/themes";
+import JournalView from "../../components/JournalView/JournalView.jsx";
+import JournalEntry from "../../components/JournalEntry/JournalEntry.jsx";
+import { Chart } from 'react-google-charts';
 
 function AppPage() {
   const navigate = useNavigate();
 
   const itemList = [
     {
-      title: 'Journal 1',
-      date: '01/01/2022',
+      title: "Journal 1",
+      date: "01/01/2022", 
       content:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     },
@@ -29,8 +30,14 @@ function AppPage() {
   };
 
   const navigateToExercise = () => {
-    navigate('/exercise');
+    navigate("/exercise");
   };
+
+  const [showChart, setShowChart] = useState(false);
+
+    const toggleChart = () => {
+        setShowChart(!showChart);
+    };
 
   return (
     <div className="app-container">
@@ -61,26 +68,84 @@ function AppPage() {
             </Text>
           </Card>
         </div>
+        
+        <Dialog.Root>
+          <div className="app-card">
+            <Card variant="classic">
+              <Inset clip="padding-box" side="top" pb="current">
+                <Dialog.Trigger>
+                  <img
+                    src="https://images.pexels.com/photos/265087/pexels-photo-265087.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                    alt="Bold typography"
+                    style={{
+                      display: "block",
+                      objectFit: "cover",
+                      width: "100%",
+                      height: 160,
+                    }}
+                    onClick={toggleChart}
+                  />
+                </Dialog.Trigger>
+              </Inset>
+              <Text as="p" size="3">
+                View Your Statistics
+              </Text>
+            </Card>
+          </div>
+          <Dialog.Content>
+            <Dialog.Title>Create a chart here</Dialog.Title>
+              <div className="graph-wrapper">
+                {showChart && (
+                  <div style={{ display: 'flex', maxWidth: 900 }}>
+                    <Chart
+                        width={'500px'}
+                        height={'300px'}
+                        chartType="LineChart"
+                        loader={<div>Loading Chart</div>}
+                        data={[
+                            ['Year', 'Sales', 'Expenses'],
+                            ['2013', 1000, 400],
+                            ['2014', 1170, 460],
+                            ['2015', 660, 1120],
+                            ['2016', 1030, 540],
+                        ]}
+                        options={{
+                            title: 'Company Performance',
+                            curveType: 'function',
+                            legend: { position: 'bottom' },
+                        }}
+                        rootProps={{ 'data-testid': '1' }}
+                    />
+                </div>
+            )}
 
-        <div className="app-card">
-          <Card variant="classic">
-            <Inset clip="padding-box" side="top" pb="current">
-              <img
-                src="https://images.pexels.com/photos/265087/pexels-photo-265087.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                alt="Bold typography"
-                style={{
-                  display: 'block',
-                  objectFit: 'cover',
-                  width: '100%',
-                  height: 160,
-                }}
-              />
-            </Inset>
-            <Text as="p" size="3">
-              View Your Statistics
-            </Text>
-          </Card>
-        </div>
+                {showChart && (
+                  <div style={{ display: 'flex', maxWidth: 900 }}>
+                    <Chart
+                        width={'500px'}
+                        height={'300px'}
+                        chartType="LineChart"
+                        loader={<div>Loading Chart</div>}
+                        data={[
+                            ['Year', 'Sales', 'Expenses'],
+                            ['2013', 1000, 400],
+                            ['2014', 1170, 460],
+                            ['2015', 660, 1120],
+                            ['2016', 1030, 540],
+                        ]}
+                        options={{
+                            title: 'Company Performance',
+                            curveType: 'function',
+                            legend: { position: 'bottom' },
+                        }}
+                        rootProps={{ 'data-testid': '1' }}
+                    />
+                </div>
+            )}
+            </div>
+          </Dialog.Content>
+
+        </Dialog.Root>
 
         <Dialog.Root>
           <div className="app-card">
